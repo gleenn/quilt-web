4fe2349a88eeb89617ecdcd74ac78dde48f48a12
Glenn Jahnke <gleenn@gmail.com>
2017-05-03 20:20:16 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to quilt-web
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/quilt_web/core_test.clj
@@ -0,0 +1,7 @@
+(ns quilt-web.core-test
+  (:require [clojure.test :refer :all]
+            [quilt-web.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
