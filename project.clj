413d67cde301df6a2a4e87852020eae82379e289
Glenn Jahnke <gleenn@gmail.com>
2017-05-03 22:31:17 -0700

    haro werld

--- project.clj
+++ project.clj
@@ -1,6 +1,39 @@
 (defproject quilt-web "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A web interface to Quilt (Regex Compressor)"
+  :url "http://regex-compressor.herokuapp.com"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :main quilt-web.core
+  :dependencies [[aleph "0.4.1-beta4"]
+                 [clj-time "0.11.0"]
+                 [com.layerware/hugsql "0.4.6"]
+                 [enlive "1.1.6"]
+                 [environ "1.0.2"]
+                 [hiccup "1.0.5"]
+                 [hiccup "1.0.5"]
+                 [metosin/compojure-api "1.0.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [ring-refresh "0.1.2"]
+                 [ring/ring-devel "1.5.0"]]
+
+  :plugins [[lein-cljfmt "0.5.6"]
+            [lein-environ "1.0.2"]]
+  :test-refresh {:changes-only true}
+  :profiles {:dev     {:source-paths ["dev"]
+                       :env          {:host            "localhost"
+                                      :reload          "true"
+                                      :port            "3000"
+                                      :database-url    "//postgres@localhost:5432/quilt-web-dev"
+                                      :bugsnag-api-key ""}
+                       :plugins      [[com.jakemccrary/lein-test-refresh "0.19.0"]
+                                      [venantius/ultra "0.5.1"]]
+                       :dependencies [[ring/ring-mock "0.3.0"]
+                                      [cljfmt/cljfmt "0.5.6"]
+                                      [org.clojure/core.async "0.2.395"]
+                                      [org.clojure/tools.namespace "0.2.3"]
+                                      ]}
+             :test    {:dependencies [[org.clojure/core.async "0.2.395"]]
+                       :env          {:database-url "//postgres@localhost:5432/"}}
+             :uberjar {:aot :all}})
--- src/quilt_web/core.clj
+++ src/quilt_web/core.clj
@@ -1,6 +1,58 @@
-(ns quilt-web.core)
+(ns quilt-web.core
+  (:gen-class)
+  (:require
+    [aleph.flow]
+    [aleph.http :as http]
+    [clojure.string :as string]
+    [environ.core :refer [env]]
+    [clojure.data.json :as json]
+    [compojure.core :refer [wrap-routes]]
+    [compojure.api.sweet :refer [context defapi GET POST ANY swagger-routes routes]]
+    [compojure.api.impl.logging :as logging]
+    [compojure.api.exception :refer [stringify-error]]
+    [hiccup.core :as hiccup]
+    [hiccup.page :as page]
+    [ring.middleware.params :refer [wrap-params]]
+    [ring.util.response :as response]
+    [ring.middleware.reload :refer [wrap-reload]]
+    [ring.middleware.refresh :refer [wrap-refresh]]
+    [ring.util.http-response :refer [ok bad-request not-found internal-server-error]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def all-routes
+  (routes
+    #_api
+    #_(GET "/.well-known/acme-challenge/:acme-token" [acme-token]
+         (let [resp (sabayon-response (sabayon-config) acme-token)]
+           (if (= (:status resp) 200)
+             (-> resp
+                 (response/content-type "text/plain; charset=utf-8"))
+             (-> (not-found (json/write-str {:error 404}))
+                 (response/content-type "application/json; charset=utf-8")))))
+
+    #_(GET "/vote-buttons" [& params]
+         (urban-api.vote-buttons/response params))
+
+    (GET "/haro" [& params]
+         "Haro werld")
+
+    (ANY "/*" []
+         (-> (not-found (json/write-str {:error 404}))
+             (response/content-type "application/json; charset=utf-8")))))
+
+(def app (-> all-routes
+             (cond-> (env :reload) wrap-reload
+                     (env :reload) wrap-refresh)))
+
+(defn create-executor []
+  (aleph.flow/utilization-executor
+    (Float/parseFloat (env :aleph-utilization "0.9"))
+    (Integer/parseInt (env :aleph-max-threads "512"))
+    {}))
+
+(defn run []
+  (http/start-server #'app {:port     (Integer/parseInt (env :port))
+                            :executor (create-executor)})
+  (println "server running on port " (Integer/parseInt (env :port))))
+
+(defn -main [& args]
+  (run))
